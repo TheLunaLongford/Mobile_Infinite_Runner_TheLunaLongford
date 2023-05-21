cc8e85ba53e276c3e752fc964ec41e80f553dc97
TheLunaLongford <fagonzalezluna@gmail.com>
2023-05-21 15:02:41 -0600

    [ADD] P2: include character jumping feature

--- Assets/Scripts/player_movement.cs
+++ Assets/Scripts/player_movement.cs
@@ -3,16 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
+[RequireComponent(typeof(Rigidbody2D))]
 public class player_movement : MonoBehaviour
 {
     [SerializeField] float move_vector;
     [SerializeField] float move_speed;
 
-    private void OnMove(InputValue value)
-    {
-        //move_vector = new Vector2((float)value.Get(Ve), 0);
-        move_vector = value.Get<float>();
+    Rigidbody2D rigib_body;
+    [SerializeField] float jump_speed;
+    [SerializeField] float gravity;
 
+
+    void OnMove(InputValue value)
+    {
+        move_vector = value.Get<float>();
+    }
+
+    void OnJump()
+    {
+        rigib_body.AddForce(Vector2.up * jump_speed, ForceMode2D.Impulse);
     }
     // Start is called before the first frame update
 
@@ -20,5 +30,13 @@ public class player_movement : MonoBehaviour
     void Update()
     {
         transform.Translate(new Vector3(move_vector, 0, 0) * move_speed * Time.deltaTime);
+    }
+
+    private void Awake()
+    {
+        rigib_body = GetComponent<Rigidbody2D>();
+        rigib_body.gravityScale = gravity = 4;
+        move_speed = 10;
+        jump_speed = 20;
     }
 }
