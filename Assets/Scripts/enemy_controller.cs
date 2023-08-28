e6e8378369518d2ec224cd141a1e06f00a363141
TheLunaLongford <fagonzalezluna@gmail.com>
2023-08-27 18:13:05 -0600

    [ADD] mobile: include a lot of thing for mobile development

--- Assets/Scripts/enemy_controller.cs
+++ Assets/Scripts/enemy_controller.cs
@@ -4,31 +4,49 @@ using UnityEngine;
 
 public class enemy_controller : MonoBehaviour
 {
-    public GameObject bullet_prefab;
-    public Transform fire_point;
+    public GameObject player;
 
-    public float fire_Rate;
-    public float next_fire_time;
+    public GameObject proyectile;
+    public Transform proyectile_spawn_point;
+    public float proyectile_speed;
+
+    public GameObject power_up_prefab;
+    public float fire_rate;
+    private int bullets_to_Skip;
+    private int bullets_skipped;
+    private float last_shoot_time;
 
 
     private void Start()
     {
-        fire_Rate = 2.0f;
-
+        fire_rate = 1.5f;
+        bullets_to_Skip = 3;
+        bullets_skipped = 0;
+        last_shoot_time = Time.time;
+        proyectile_speed = 10.0f;
     }
 
     private void Update()
     {
-        if (Time.time > next_fire_time)
+        if (Time.time - last_shoot_time >= fire_rate)
         {
-            shoot();
-            next_fire_time = Time.time + 1.0f / fire_Rate;
+            Shoot();
+            last_shoot_time = Time.time;
         }
     }
 
-    private void shoot()
+    private void Shoot()
     {
-        Instantiate(bullet_prefab, fire_point.position, fire_point.rotation);
+        Vector3 shoot_direction = (player.transform.position - transform.position).normalized;
+        GameObject balita = Instantiate(proyectile, proyectile_spawn_point.position, Quaternion.identity);
+        Rigidbody2D rb = balita.GetComponent<Rigidbody2D>();
+        rb.velocity = shoot_direction * proyectile_speed;
     }
+
+    private void drop_power_up()
+    {
+        Instantiate(power_up_prefab, transform.position, Quaternion.identity);
+    }
+
 
 }
